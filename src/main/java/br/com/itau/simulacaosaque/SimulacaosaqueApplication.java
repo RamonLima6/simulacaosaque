3cef15e90ce0498b302edbfd8aa18c4f15dc3c65
RamonUNG6 <ramon_andrade_lima@hotmail.com>
2025-02-13 22:09:14 -0300

    First commit

---
+++ src/main/java/br/com/itau/simulacaosaque/SimulacaosaqueApplication.java
@@ -0,0 +1,13 @@
+package br.com.itau.simulacaosaque;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimulacaosaqueApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SimulacaosaqueApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/itau/simulacaosaque/SimulacaosaqueApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.itau.simulacaosaque;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimulacaosaqueApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
